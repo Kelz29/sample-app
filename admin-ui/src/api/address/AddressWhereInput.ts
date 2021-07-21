c839c9266ab99018e93bd116eb4b8d53d97fc144
Kelello <makamanekj@gmail.com>
2021-07-21 19:50:03 +0200

    amplication-build-ckrds6doi15470411bs6ojckmqud

---
+++ admin-ui/src/api/address/AddressWhereInput.ts
@@ -0,0 +1,13 @@
+import { StringNullableFilter } from "../../util/StringNullableFilter";
+import { StringFilter } from "../../util/StringFilter";
+import { IntNullableFilter } from "../../util/IntNullableFilter";
+
+export type AddressWhereInput = {
+  address_1?: StringNullableFilter;
+  address_2?: StringNullableFilter;
+  address_3?: StringNullableFilter;
+  city?: StringNullableFilter;
+  id?: StringFilter;
+  state?: StringNullableFilter;
+  zip?: IntNullableFilter;
+};
